143cdc003108553464ab42d786f7c8385c54a6be
mrotoliluiz <mrotoliluiz@gmail.com>
2026-07-10 12:30:06 -0300

    inicio do inicio

--- Readduo/Readduo/src/pages/home/index.jsx
+++ Readduo/Readduo/src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import styles from "./index.module.css";
 import Sidebar from "../../componentes/Sidebar";
+import {Flame, Star, House, PenTool, BookOpen } from "lucide-react";
 
 function Home() {
 
@@ -8,9 +9,10 @@ function Home() {
 
       
 
-        <header className={styles.header}>
+        <div className={styles.header}>
+          <House/>
           <h1>Início</h1>
-        </header>
+        </div>
 
         <section className={styles.welcome}>
 
@@ -18,28 +20,32 @@ function Home() {
             <h2>Olá, Luna! </h2>
 
             <p>
-              Continue sua jornada literária.
+              Continue sua jornada literária. Você está indo muito bem!!
             </p>
           </div>
 
           <div className={styles.stats}>
 
             <div className={styles.statCard}>
+              <BookOpen/>
               <h3>3</h3>
               <span>Livros</span>
             </div>
 
             <div className={styles.statCard}>
+              <PenTool/>
               <h3>4.2K</h3>
               <span>Palavras</span>
             </div>
 
             <div className={styles.statCard}>
+              <Flame/>
               <h3>8 dias</h3>
               <span>Sequência</span>
             </div>
 
             <div className={styles.statCard}>
+              <Star/>
               <h3>+850</h3>
               <span>XP</span>
             </div>
