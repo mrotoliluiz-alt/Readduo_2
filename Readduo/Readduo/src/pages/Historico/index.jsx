5d183be63653720dcbccf7b53f314de5dffcff53
mrotoliluiz <mrotoliluiz@gmail.com>
2026-07-20 18:08:50 -0300

    inicio e Histórico brutos

--- Readduo/Readduo/src/pages/Historico/index.jsx
+++ Readduo/Readduo/src/pages/Historico/index.jsx
@@ -1,8 +1,180 @@
-import styles from './index.module.css';
-import { href, useNavigate } from 'react-router-dom';
+import styles from "./index.module.css";
 
-export default function Historico(){
+import {
+    History,
+    BookOpen,
+    Clock3,
+    PenTool,
+    Trophy,
+    Star,
+    ArrowRight,
+    MoreVertical,
+} from "lucide-react";
+
+const cards = [
+    {
+        icon: <BookOpen color="#61a6fa"/>,
+        titulo: "Livros lidos",
+        valor: "48",
+        mes: "+5 este mês"
+    },
+    {
+        icon: <Clock3 color="#b05cff"/>,
+        titulo: "Horas de leitura",
+        valor: "126h",
+        mes: "+18h este mês"
+    },
+    {
+        icon: <PenTool color="#4ade80"/>,
+        titulo: "Obras escritas",
+        valor: "7",
+        mes: "+2 este mês"
+    },
+    {
+        icon: <Trophy color="#ffdb0c"/>,
+        titulo: "Conquistas",
+        valor: "24",
+        mes: "+4 este mês"
+    },
+    {
+        icon: <Star color="#2ea9ff"/>,
+        titulo: "XP acumulado",
+        valor: "24.850",
+        mes: "+2.450 este mês"
+    },
+];
+
+const livros = [
+    {
+        capa:"img/duna.jpg",
+        nome:"Duna",
+        autor:"Frank Herbert",
+        genero:"Ficção Científica",
+        paginas:"688 páginas",
+        data:"12/05/2024",
+        nota:"★★★★★"
+    },
+    {
+        capa:"img/jogos.jpg",
+        nome:"Jogos Vorazes",
+        autor:"Suzanne Collins",
+        genero:"Aventura",
+        paginas:"374 páginas",
+        data:"05/05/2024",
+        nota:"★★★★☆"
+    }
+];
+
+function Historico(){
+
     return(
-        <h1>Histórico </h1>
+
+        <div className={styles.container}>
+
+            <div className={styles.header}>
+
+                <History/>
+
+                <div>
+
+                    <h1>Histórico</h1>
+
+                    <p>
+                        Revise sua jornada, celebre conquistas e continue evoluindo.
+                    </p>
+
+                </div>
+
+            </div>
+
+            <section className={styles.cards}>
+
+                {cards.map((card,index)=>(
+                    <div key={index} className={styles.card}>
+
+                        {card.icon}
+
+                        <span>{card.titulo}</span>
+
+                        <h2>{card.valor}</h2>
+
+                        <p>{card.mes}</p>
+
+                    </div>
+                ))}
+
+            </section>
+
+            <div className={styles.titleSection}>
+
+                <h2>Histórico de Leituras</h2>
+
+                <button>
+
+                    Ver todas
+
+                    <ArrowRight size={18}/>
+
+                </button>
+
+            </div>
+
+            <section className={styles.listaLivros}>
+
+                {livros.map((livro,index)=>(
+
+                    <div className={styles.livro} key={index}>
+
+                        <img src={livro.capa}/>
+
+                        <div className={styles.info}>
+
+                            <h3>{livro.nome}</h3>
+
+                            <p>{livro.autor}</p>
+
+                            <span className={styles.genero}>
+                                {livro.genero}
+                            </span>
+
+                            <small>{livro.paginas}</small>
+
+                        </div>
+
+                        <div className={styles.progresso}>
+
+                            <p>Concluído em {livro.data}</p>
+
+                            <div className={styles.barra}>
+
+                                <div className={styles.preenchimento}></div>
+
+                            </div>
+
+                            <span>100%</span>
+
+                        </div>
+
+                        <button className={styles.botao}>
+                            Ver resumo
+                        </button>
+
+                        <div className={styles.nota}>
+                            {livro.nota}
+                        </div>
+
+                        <MoreVertical/>
+
+                    </div>
+
+                ))}
+
+            </section>
+
+        </div>
+
     )
+
 }
+
+export default Historico;
--- Readduo/Readduo/src/pages/home/index.jsx
+++ Readduo/Readduo/src/pages/home/index.jsx
@@ -17,7 +17,7 @@ function Home() {
         <section className={styles.welcome}>
 
           <div>
-            <h2>Olá, Luna! </h2>
+            <h2>Olá, nome do usuario</h2>
 
             <p>
               Continue sua jornada literária. Você está indo muito bem!!
@@ -27,25 +27,25 @@ function Home() {
           <div className={styles.stats}>
 
             <div className={styles.statCard}>
-              <BookOpen/>
+              <BookOpen color="#61a6fa"/>
               <h3>3</h3>
-              <span>Livros</span>
+              <span>Lidos</span>
             </div>
 
             <div className={styles.statCard}>
-              <PenTool/>
+              <PenTool color="#4ade80"/>
               <h3>4.2K</h3>
               <span>Palavras</span>
             </div>
 
             <div className={styles.statCard}>
-              <Flame/>
+              <Flame color="#fb923c"/>
               <h3>8 dias</h3>
               <span>Sequência</span>
             </div>
 
             <div className={styles.statCard}>
-              <Star/>
+              <Star color="#ffdb0c"/>
               <h3>+850</h3>
               <span>XP</span>
             </div>
@@ -54,9 +54,41 @@ function Home() {
 
         </section>
 
-        <section>
-          <h2>Continue lendo</h2>
-        </section>
+          <h2>Explore novos Caminhos</h2>
+
+          <div className={styles.leitCard}>
+
+            <div className={styles.card}>
+            <img src="img.jpg" alt="imagem aleatoria"  className={styles.imgCard}/>
+            <h2>Texto Exemplo:</h2>
+            <p className={styles.descricao}> Exemplo de descricao</p>
+            </div>
+
+            <div className={styles.card}>
+            <img src="img.jpg" alt="imagem aleatoria"  className={styles.imgCard}/>
+            <h2>Texto Exemplo:</h2>
+            <p className={styles.descricao}> Exemplo de descricao</p>
+            </div>
+
+            <div className={styles.card}>
+            <img src="img.jpg" alt="imagem aleatoria"  className={styles.imgCard}/>
+            <h2>Texto Exemplo:</h2>
+            <p className={styles.descricao}> Exemplo de descricao</p>
+            </div>
+
+            <div className={styles.card}>
+            <img src="img.jpg" alt="imagem aleatoria"  className={styles.imgCard}/>
+            <h2>Texto Exemplo:</h2>
+            <p className={styles.descricao}> Exemplo de descricao</p>
+            </div>
+
+            <div className={styles.card}>
+            <img src="img.jpg" alt="imagem aleatoria"  className={styles.imgCard}/>
+            <h2>Texto Exemplo:</h2>
+            <p className={styles.descricao}> Exemplo de descricao</p>
+            </div>
+
+          </div>
 
       </div>
 
